c2e6fedef16133725dad42b52c4d0bf021908b45
Andile-Maphalala <andile.maphalala.am@gmail.com>
2025-10-27 21:06:22 +0200

    Created a custom table that will display the mudtable  with action columns Added needed services to get dialogs and other things to run in the app

--- SavingTracker.UI/SavingTracker.UI/Models/CRUDs/SavingsPlanModel.cs
+++ SavingTracker.UI/SavingTracker.UI/Models/CRUDs/SavingsPlanModel.cs
@@ -1,12 +1,27 @@
-﻿namespace SavingTracker.UI.Models.CRUDs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SavingTracker.UI.Models.CRUDs
 {
     public class SavingsPlanModel
     {
+        [Display(Name = "Id", Order = 0)]
+
         public int Id { get; set; }
+
+        [Display(Name = "Name", Order = 1)]
         public string Name { get; set; }
+
+
+        [Display(Name = "Description", Order = 2)]
         public string? Description { get; set; }
+
+        [Display(Name = "TargetAmount", Order = 3)]
         public decimal TargetAmount { get; set; }
+
+        [Display(Name = "StartDate", Order = 4)]
         public DateTime StartDate { get; set; }
+
+        [Display(Name = "EndDate", Order = 5)]
         public DateTime? EndDate { get; set; }
     }
 }
