d384c644d690d287b66a4021d02da837c9e8a3cc
Andile-Maphalala <andile.maphalala.am@gmail.com>
2025-10-29 20:50:54 +0200

    contrbution crud

--- SavingTracker.UI/SavingTracker.UI/Models/CRUDs/ContributionModel.cs
+++ SavingTracker.UI/SavingTracker.UI/Models/CRUDs/ContributionModel.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int MemberId { get; set; }
         public string MemberName { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; }
         public decimal Amount { get; set; }
     }
 }
