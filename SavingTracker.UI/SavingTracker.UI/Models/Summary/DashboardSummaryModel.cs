226b6966822ff90354895402a41d4467dbc95d7c
Andile-Maphalala <andile.maphalala.am@gmail.com>
2025-10-30 20:44:30 +0200

    Added total periods

--- SavingTracker.UI/SavingTracker.UI/Models/Summary/DashboardSummaryModel.cs
+++ SavingTracker.UI/SavingTracker.UI/Models/Summary/DashboardSummaryModel.cs
@@ -6,6 +6,8 @@
         public decimal TotalContribution { get; set; }
         public decimal TotalOutstandingAmount { get; set; }
         public decimal PeriodIncome { get; set; }
+        public int TotalPeriods { get; set; }
+
         public List<MemberDashboardSummaryModel> MemberSummaries { get; set; }
 
     }
